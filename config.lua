26b1c80acd2d666ade3aa6b1edb967a24ffcfd6e
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-06-24 13:04:12 +0200

    Changed to vector values

--- config.lua
+++ config.lua
@@ -1,79 +1,36 @@
 QBBoatshop = QBBoatshop or {}
 QBDiving = QBDiving or {}
 
-QBBoatshop.PoliceBoat = {
-    x = -800.67,
-    y = -1494.54,
-    z = 1.59,
+QBBoatshop.PoliceBoat = vector3(-800.67, -1494.54, 1.59)
+
+QBBoatshop.PoliceBoatSpawn = vector4(-793.58, -1501.4, 0.12, 111.5)
 }
 
-QBBoatshop.PoliceBoatSpawn = {
-    x = -793.58,
-    y = -1501.4,
-    z = 0.12,
-    h = 111.5,
-}
+QBBoatshop.PoliceBoat2 = vector3(-279.41, 6635.09, 7.51)
 
-QBBoatshop.PoliceBoat2 = {
-    x = -279.41,
-    y = 6635.09,
-    z = 7.51,
-}
 
-QBBoatshop.PoliceBoatSpawn2 = {
-    x = -293.10,
-    y = 6642.69,
-    z = 0.15,
-    h = 65.5,
-}
+QBBoatshop.PoliceBoatSpawn2 = vector4(-293.10, 6642.69, 0.15, 65.5)
 
 QBBoatshop.Docks = {
     ["lsymc"] = {
         label = "LSYMC Boathouse",
         coords = {
-            take = {
-                x = -794.66,
-                y = -1510.83,
-                z = 1.59,
-            },
-            put = {
-                x = -793.58,
-                y = -1501.4,
-                z = 0.12,
-                h = 111.5,
-            }
+            take = vector3(-794.66, -1510.83, 1.59),
+            put = vector4(-793.58, -1501.4, 0.12, 111.5)
         }
     },
     ["paletto"] = {
         label = "Paleto Boathouse",
         coords = {
-            take = {
-                x = -277.46,
-                y = 6637.2,
-                z = 7.48,
-            },
-            put = {
-                x = -289.2,
-                y = 6637.96,
-                z = 1.01,
-                h = 45.5,
-            }
+            take = vector3(-277.46, 6637.2, 7.48),
+            put = vector4(-289.2, 6637.96, 1.01, 45.5)
         }
     },
     ["millars"] = {
         label = "Millars Boathouse",
         coords = {
-            take = {
-                x = 1299.24,
-                y = 4216.69,
-                z = 33.9,
-            },
-            put = {
-                x = 1297.82,
-                y = 4209.61,
-                z = 30.12,
-                h = 253.5,
-            }
+            take = vector3(1299.24, 4216.69, 33.9),
+            put = vector4(1297.82, 4209.61, 30.12, 253.5),
         }
     },
 }
@@ -82,17 +39,8 @@ QBBoatshop.Depots = {
     [1] = {
         label = "LSYMC Depot",
         coords = {
-            take = {
-                x = -772.98,
-                y = -1430.76,
-                z = 1.59,
-            },
-            put = {
-                x = -729.77,
-                y = -1355.49,
-                z = 1.19,
-                h = 142.5,
-            }
+            take = vector3(-772.98, -1430.76, 1.59),
+            put = vector4(-729.77, -1355.49, 1.19, 142.5)
         }
     },
 }
@@ -102,68 +50,32 @@ QBBoatshop.Locations = {
         [1] = {
             ["boatModel"] = "seashark",
             ["coords"] = {
-                ["boat"] = {
-                    ["x"] = -727.05,
-                    ["y"] = -1326.59,
-                    ["z"] = 1.06,
-                    ["h"] = 229.5
-                },
-                ["buy"] = {
-                    ["x"] = -723.3,
-                    ["y"] = -1323.61,
-                    ["z"] = 1.59,
-                }
+                ["boat"] = vector4(-727.05, -1326.59, 1.06, 229.5),
+                ["buy"] = vector3(-723.3, -1323.61, 1.59),
             },
             ["inUse"] = false
         },
         [2] = {
             ["boatModel"] = "dinghy",
             ["coords"] = {
-                ["boat"] = {
-                    ["x"] = -732.84,
-                    ["y"] = -1333.5,
-                    ["z"] = 1.59,
-                    ["h"] = 229.5
-                },
-                ["buy"] = {
-                    ["x"] = -729.19,
-                    ["y"] = -1330.58,
-                    ["z"] = 1.67,
-                },
+                ["boat"] = vector4(-732.84, -1333.5, 1.59, 229.5),
+                ["buy"] = vector3(-729.19, -1330.58, 1.67),
             },
             ["inUse"] = false
         },
         [3] = {
             ["boatModel"] = "speeder",
             ["coords"] = {
-                ["boat"] = {
-                    ["x"] = -737.84,
-                    ["y"] = -1340.83,
-                    ["z"] = 0.79,
-                    ["h"] = 229.5
-                },
-                ["buy"] = {
-                    ["x"] = -734.98,
-                    ["y"] = -1337.42,
-                    ["z"] = 1.67,
-                },
+                ["boat"] = vector4(-737.84, -1340.83, 0.79, 229.5),
+                ["buy"] = vector3(-734.98, -1337.42, 1.67),
             },
             ["inUse"] = false
         },
         [4] = {
             ["boatModel"] = "marquis",
             ["coords"] = {
-                ["boat"] = {
-                    ["x"] = -741.53,
-                    ["y"] = -1349.7,
-                    ["z"] = 0.79,
-                    ["h"] = 229.5
-                },
-                ["buy"] = {
-                    ["x"] = -740.62,
-                    ["y"] = -1344.28,
-                    ["z"] = 1.67,
-                },
+                ["boat"] = vector4(-741.53, -1349.7, 0.79, 229.5),
+                ["buy"] = vector3(-740.62, -1344.28, 1.67),
             },
             ["inUse"] = false
         },
@@ -329,53 +241,28 @@ QBBoatshop.ShopBoats = {
     }
 }
 
-QBBoatshop.SpawnVehicle = {
-    x = -729.77,
-    y = -1355.49,
-    z = 1.19,
-    h = 142.5,
-}
+QBBoatshop.SpawnVehicle = vector4(-729.77, -1355.49, 1.19, 142.5)
 
 QBDiving.Locations = {
     [1] = {
         label = "This is Location 1",
         coords = {
-            Area = {
-                x = -2838.8,
-                y = -376.1,
-                z = 3.55
-            },
+            Area = vector3(-2838.8, -376.1, 3.55),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = -2849.25,
-                        y = -377.58,
-                        z = -40.23
-                    },
+                    coords = vector3(-2849.25, -377.58, -40.23),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = -2838.43,
-                        y = -363.63,
-                        z = -39.45
-                    },
+                    coords = vector3(-2838.43, -363.63, -39.45),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = -2887.04,
-                        y = -394.87,
-                        z = -40.91
-                    },
+                    coords = vector3(-2887.04, -394.87, -40.91),
                     PickedUp = false
                 },
                 [4] = {
-                    coords = {
-                        x = -2808.99,
-                        y = -385.56,
-                        z = -39.32
-                    },
+                    coords = vector3(-2808.99, -385.56, -39.32),
                     PickedUp = false
                 },
             }
@@ -386,34 +273,18 @@ QBDiving.Locations = {
     [2] = {
         label = "Location 2",
         coords = {
-            Area = {
-                x = -3288.2,
-                y = -67.58,
-                z = 2.79,
-            },
+            Area = vector3(-3288.2, -67.58, 2.79),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = -3275.03,
-                        y = -38.58,
-                        z = -19.21,
-                    },
+                    coords = vector3(-3275.03, -38.58, -19.21),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = -3273.73,
-                        y = -76.0,
-                        z = -26.81,
-                    },
+                    coords = vector3(-3273.73, -76.0, -26.81),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = -3346.53,
-                        y = -50.4,
-                        z = -35.84
-                    },
+                    coords = vector3(-3346.53, -50.4, -35.84),
                     PickedUp = false
                 },
             }
@@ -424,34 +295,18 @@ QBDiving.Locations = {
     [3] = {
         label = "Location 3",
         coords = {
-            Area = {
-                x = -3367.24,
-                y = 1617.89,
-                z = 1.39,
-            },
+            Area = vector3(-3367.24, 1617.89, 1.39),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = -3388.01,
-                        y = 1635.88,
-                        z = -39.41,
-                    },
+                    coords = vector3(-3388.01, 1635.88, -39.41),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = -3354.19,
-                        y = 1549.3,
-                        z = -38.21,
-                    },
+                    coords = vector3(-3354.19, 1549.3, -38.21),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = -3326.04,
-                        y = 1636.43,
-                        z = -40.98
-                    },
+                    coords = vector3(-3326.04, 1636.43, -40.98),
                     PickedUp = false
                 },
             }
@@ -462,34 +317,18 @@ QBDiving.Locations = {
     [4] = {
         label = "Location 4",
         coords = {
-            Area = {
-                x = 3002.5,
-                y = -1538.28,
-                z = -27.36,
-            },
+            Area = vector3(3002.5, -1538.28, -27.36),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = 2978.05,
-                        y = -1509.07,
-                        z = -24.96,
-                    },
+                    coords = vector3(2978.05, -1509.07, -24.96),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = 3004.42,
-                        y = -1576.95,
-                        z = -29.36,
-                    },
+                    coords = vector3(3004.42, -1576.95, -29.36),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = 2951.65,
-                        y = -1560.69,
-                        z = -28.36,
-                    },
+                    coords = vector3(2951.65, -1560.69, -28.36),
                     PickedUp = false
                 },
             }
@@ -500,42 +339,22 @@ QBDiving.Locations = {
     [5] = {
         label = "Location 5",
         coords = {
-            Area = {
-                x = 3421.58,
-                y = 1975.68,
-                z = 0.86,
-            },
+            Area = vector3(3421.58, 1975.68, 0.86),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = 3421.69,
-                        y = 1976.54,
-                        z = -50.64,
-                    },
+                    coords = vector3(3421.69, 1976.54, -50.64),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = 3424.07,
-                        y = 1957.46,
-                        z = -53.04,
-                    },
+                    coords = vector3(3424.07, 1957.46, -53.04),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = 3434.65,
-                        y = 1993.73,
-                        z = -49.84,
-                    },
+                    coords = vector3(3434.65, 1993.73, -49.84),
                     PickedUp = false
                 },
                 [4] = {
-                    coords = {
-                        x = 3415.42,
-                        y = 1965.25,
-                        z = -52.04,
-                    },
+                    coords = vector3(3415.42, 1965.25, -52.04),
                     PickedUp = false
                 },
             }
@@ -546,42 +365,22 @@ QBDiving.Locations = {
     [6] = {
         label = "Location 6",
         coords = {
-            Area = {
-                x = 2720.14,
-                y = -2136.28,
-                z = 0.74,
-            },
+            Area = vector3(2720.14, -2136.28, 0.74),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = 2724.0,
-                        y = -2134.95,
-                        z = -19.33,
-                    },
+                    coords = vector3(2724.0, -2134.95, -19.33),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = 2710.68,
-                        y = -2156.06,
-                        z = -18.63,
-                    },
+                    coords = vector3(2710.68, -2156.06, -18.63),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = 2702.84,
-                        y = -2139.29,
-                        z = -18.51,
-                    },
+                    coords = vector3(2702.84, -2139.29, -18.51),
                     PickedUp = false
                 },
                 [4] = {
-                    coords = {
-                        x = 2736.27,
-                        y = -2153.91,
-                        z = -20.88,
-                    },
+                    coords = vector3(2736.27, -2153.91, -20.88),
                     PickedUp = false
                 },
             }
@@ -592,42 +391,22 @@ QBDiving.Locations = {
     [7] = {
         label = "Location 7",
         coords = {
-            Area = {
-                x = 536.69,
-                y = 7253.75,
-                z = 1.69,
-            },
+            Area = vector3(536.69, 7253.75, 1.69),
             Coral = {
                 [1] = {
-                    coords = {
-                        x = 542.31,
-                        y = 7245.37,
-                        z = -30.01,
-                    },
+                    coords = vector3(542.31, 7245.37, -30.01),
                     PickedUp = false
                 },
                 [2] = {
-                    coords = {
-                        x = 528.21,
-                        y = 7223.26,
-                        z = -29.51,
-                    },
+                    coords = vector3(528.21, 7223.26, -29.51),
                     PickedUp = false
                 },
                 [3] = {
-                    coords = {
-                        x = 510.36,
-                        y = 7254.97,
-                        z = -32.11,
-                    },
+                    coords = vector3(510.36, 7254.97, -32.11),
                     PickedUp = false
                 },
                 [4] = {
-                    coords = {
-                        x = 525.37,
-                        y = 7259.12,
-                        z = -30.51,
-                    },
+                    coords = vector3(525.37, 7259.12, -30.51),
                     PickedUp = false
                 },
             }
@@ -653,15 +432,10 @@ QBDiving.CoralTypes = {
 
 QBDiving.SellLocations = {
     [1] = {
-    ["coords"] = {
+    ["coords"] = vector3(-1684.13, -1068.91, 13.15)
         -- ["x"] = -1684.13,
         -- ["y"] = -1068.91,
         -- ["z"] = 13.15
-        x = -1684.13,
-        y = -1068.91,
-        z = 13.15,
-
-    }
 }
 }
 
