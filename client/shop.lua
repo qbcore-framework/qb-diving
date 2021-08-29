b46db553fc9bb26c2e9d60a8f94ebf94fcb62ac7
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-08-29 20:25:07 +0200

    Fix inventory staying disabled

--- client/shop.lua
+++ client/shop.lua
@@ -1,5 +1,3 @@
-
-
 local notInteressted = false
 
 Citizen.CreateThread(function()
@@ -11,33 +9,26 @@ Citizen.CreateThread(function()
         if not notInteressted then
             for k, v in pairs(QBDiving.SellLocations) do
                 local dist = #(pos - vector3(v["coords"]["x"], v["coords"]["y"], v["coords"]["z"]))
-                
+
                 if dist < 20 then
                     inRange = true
-
-
                     if dist < 1 then
-                        
                         DrawText3D(v["coords"]["x"], v["coords"]["y"], v["coords"]["z"] - 0.1, '~g~G~w~ - Selling Coral')
-                        
-                            if IsControlJustPressed(0, 47) then
-                    
-                                    TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_STAND_IMPATIENT", 0, true)
-                                LocalPlayer.state:set("inv_busy", true, true)
-                                    QBCore.Functions.Progressbar("sell_coral_items", "Check Pockets To Sell Coral", math.random(2000, 4000), false, true, {}, {}, {}, {}, function() -- Done
-                                        ClearPedTasks(PlayerPedId())
-                                        TriggerServerEvent('qb-diving:server:SellCoral')
-                                        notInteressted = true
-                                        SetTimeout(0, ClearTimeOut)
-                                    end, function() -- Cancel
-                                        LocalPlayer.state:set("inv_busy", true, true)
-                                        ClearPedTasks(PlayerPedId())
-                                        QBCore.Functions.Notify("Canceled..", "error")
-                                    end)
-                               
-                                
-                            end
-                        
+                        if IsControlJustPressed(0, 47) then
+                            LocalPlayer.state:set("inv_busy", true, true)
+                            TaskStartScenarioInPlace(PlayerPedId(), "WORLD_HUMAN_STAND_IMPATIENT", 0, true)
+                            QBCore.Functions.Progressbar("sell_coral_items", "Check Pockets To Sell Coral", math.random(2000, 4000), false, true, {}, {}, {}, {}, function() -- Done
+                                ClearPedTasks(PlayerPedId())
+                                TriggerServerEvent('qb-diving:server:SellCoral')
+                                notInteressted = true
+                                SetTimeout(0, ClearTimeOut)
+                                LocalPlayer.state:set("inv_busy", false, true)
+                            end, function() -- Cancel
+                                ClearPedTasks(PlayerPedId())
+                                QBCore.Functions.Notify("Canceled..", "error")
+                                LocalPlayer.state:set("inv_busy", false, true)
+                            end)
+                        end
                     end
                 end
             end
@@ -52,7 +43,6 @@ Citizen.CreateThread(function()
         Citizen.Wait(3)
     end
 end)
-
 
 function ClearTimeOut()
     notInteressted = not notInteressted
