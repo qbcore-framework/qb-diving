7392bebfd38221fe65af7e04e5a9e60d7c1e9350
R0adRa93 <github.com@legendsclan.org>
2023-01-04 02:36:10 -0500

    Update fxmanifest.lua
    added missing polyzone data and upped the version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-Diving'
-version '1.2.0'
+version '1.2.1'
 
 shared_script {
     '@qb-core/shared/locale.lua',
@@ -16,6 +16,8 @@ server_script 'server/main.lua'
 client_scripts {
     '@PolyZone/client.lua',
     '@PolyZone/BoxZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua'
 }
 
